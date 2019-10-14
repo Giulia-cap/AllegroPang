1c75f9430de3ef01633e6470aac3343ea5a452e5
giuls-cap <giulia_caputo97@hotmail.it>
2019-10-14 20:41:20 +0200

    niente

--- Player.h
+++ Player.h
@@ -1,10 +1,16 @@
 #include "DynamicObject.h"
+#include <vector>
 #ifndef Player_H
 #define Player_H
 
 class Player:public DynamicObject
 {
-
+	/*private:
+		vector<ALLEGRO_BITMAP *> imagedx;
+		vector<ALLEGRO_BITMAP *> imagesx;
+		int movDx=0;
+		int moSx=0;
+		bool dx=false,sx=false;*/
 	public:
 		Player(int,Type);
 		~Player();
--- player.cpp
+++ player.cpp
@@ -12,8 +12,12 @@ Player::Player(int l,Type t):DynamicObject(l,t)
 	/*ALLEGRO_PATH *path = al_get_standard_path(ALLEGRO_RESOURCES_PATH); //--> funziona anche così
  	al_set_path_filename(path,"./resources/player.bmp");
  	image = al_load_bitmap(al_path_cstr(path, '/'));*/
+	/*image=al_load_bitmap("./resources/player/.dx1.png");
+	imagedx.push_back(image);
+	image=al_load_bitmap("./resources/player/dx2.png");
+	imagedx.push_back(image);  */
+	image=al_load_bitmap("./resources/player/player.bmp");
 
-	image=al_load_bitmap("./resources/player.bmp"); 
 	if(!image)
 		cout<<"ERROR TO LOAD IMMAGE";
 }
@@ -25,17 +29,34 @@ Player::~Player()
 void Player::move(int SCREEN_W,int k)
 {
    if(k==0 && bouncer_x >= 4.0) //MOVIMENTO A SINISTRA
+   {
+   	/*  dx=false;
+   	  sx=true;
+   	  movDx=0;*/
       bouncer_x -= 4.0;
+   }
    
    if(k==1 && bouncer_x <= SCREEN_W - BOUNCER_SIZE - 4.0) //MOVIMENTO A DESTRA
+   {
+   	  /*if(movDx<imagedx.size())
+   	  	movDx++;
+   	  else movDx=0;
+   	  dx=true;
+   	  sx=false;*/
       bouncer_x += 4.0;
+   }
+
+   //cout<<movDx<<endl;
    
 }
 
 void Player::tick(){}
 void Player::render()
 {
-  al_draw_bitmap(image, bouncer_x, bouncer_y, 0);
+	//if(sx)
+  		al_draw_bitmap(image, bouncer_x, bouncer_y, 0);
+  	//else if(dx)
+  		//al_draw_bitmap(imagedx[movDx],bouncer_x, bouncer_y, 0);
 }
 void Player::loadImage(){}
 void Player::die(){}
