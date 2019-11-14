55e3ccbba3f0f038bb0925e631d5344dc483cf85
Limitless994 <davide.sacco94@yahoo.com>
2019-11-14 19:42:10 +0100

    aggiunta intro

--- MenuState.cpp
+++ MenuState.cpp
@@ -25,15 +25,55 @@ if(!al_init_image_addon()) {
          fprintf(stderr, "failed to create image!\n");
         return ;
      }
-	schermata=al_load_bitmap("./resources/press.png");
-	if(!schermata)
-		cout<<"no image";
+  //ALLEGRO_SAMPLE *intro_music = al_load_sample("resources/intro_music.ogg");
+  ALLEGRO_BITMAP *image = NULL;
+  std::string count = "1";
+  std::string name = "resources/intro/intro";
+  std::string png = ".png";
+  std::string all_name;
+  std::string buffer;
+  bool done = false;
+  int c = 0, i = 0;
+
+  ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+  al_register_event_source(event_queue, al_get_timer_event_source(timer));
+  al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+ // al_reserve_samples(1);
+  //al_play_sample(intro_music, 1, 0, 1, ALLEGRO_PLAYMODE_ONCE, 0);
+
+  while (!done)
+  {
+    ALLEGRO_EVENT ev;
+    al_wait_for_event(event_queue, &ev);
+    if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+    {
+      if (ev.keyboard.keycode == ALLEGRO_KEY_SPACE)
+        done = true;
+    }
+    else if (ev.type == ALLEGRO_EVENT_TIMER)
+    {
+      all_name = name + count + png;
+      cout<<all_name<<endl;
+      image = al_load_bitmap(all_name.c_str());
+      al_draw_bitmap(image, 0, 0, 0);
+      al_flip_display();
+      al_clear_to_color(al_map_rgb(0, 0, 0));
+      al_destroy_bitmap(image);
+      c++;
+      count.clear();
+      count = std::to_string(c);
+      count = buffer + count;
+      ++i;
+    }
+    if (i == 60)
+      done = true;
+  }
+  //al_destroy_sample(intro_music);
+schermata=al_load_bitmap("./resources/press.png");
+  if(!schermata)
+    cout<<"no image";
    al_init_video_addon();
-
-   video=al_open_video("./resources/introvideo.ogv");
-   if(!video) cout<<"NO VIDEO";
-
-    al_register_event_source(event_queue, al_get_video_event_source(video));
     al_clear_to_color(al_map_rgb(0,0,0));
 
      al_start_timer(timer); 
